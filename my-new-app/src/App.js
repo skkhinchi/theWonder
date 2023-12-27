c4880711febef154253de5d045b13a2a81926873
skkhinchi <khinchi.skumar@gmail.com>
2023-12-27 10:17:53 +0530

    last-phase of 2023

---
+++ my-new-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { MyApp } from './Javascript Concepts/MyApp';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyApp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-new-app/src/Javascript Concepts/MyApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MyApp = () => {
+    console.log("Hello");
+    console.log("Hello 2");
+  return (
+    <div>Hello From My JS App </div>
+  )
+}
